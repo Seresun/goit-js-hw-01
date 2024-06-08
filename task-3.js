1d2875583a85ee16e504f0a3ee6b6450d6808c5a
Sol <slavnyunstoppable22@gmail.com>
2024-06-08 15:10:43 +0400

    version v0.1

--- task-3.js
+++ task-3.js
@@ -1,7 +1,13 @@
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+function getElementWidth(content, padding, border) {
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
+
+  const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
+
+  return totalWidth;
 }
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
